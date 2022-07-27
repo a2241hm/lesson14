bb0a705d46fa84fffab88e9771de68921d8f9ca1
Hiroki Murakami <a2241hm@aiit.ac.jp>
2022-07-27 21:57:16 +0900

    シェル内容更新

--- same-test.sh
+++ same-test.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+tmp=/tmp/$$
+
+echo "input 2 arguments" > $tmp-args
+echo "input natural number" > $tmp-nat
+
+ERROR_EXIT(){
+	echo "$1" > &2
+	rm -f $tmp-*
+	exit 1
+}
+
+#start test
+# test1 引数の数が足りない
+./same.sh 2> $tmp-ans && ERROR_EXIT "error in test1-1"
+diff $tmp-ans $tmp-args || ERROR_EXIT "error in test1-2"
--- same.sh
+++ same.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+# 引数の数判定
+#  失敗時: input 2 argments
+# 数値かどうか判定
+#  失敗時: input natural number"
+#動作開始
+#  同じ場合:$1 is equal to $2 
+#  違う場合:$1 is not equal to $2
+
+
+
